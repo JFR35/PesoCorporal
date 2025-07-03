090de914c4f52d60f6767537032fd29ca5142766
Juan <juanfajardoreyes@hotmail.com>
2025-07-03 15:16:24 +0200

    Initial Commit

---
+++ src/main/java/com/pesocorporalehr/PesocorporalApplication.java
@@ -0,0 +1,13 @@
+package com.pesocorporalehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesocorporalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesocorporalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesocorporalehr/PesocorporalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesocorporalehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesocorporalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
